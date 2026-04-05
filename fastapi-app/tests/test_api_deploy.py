e37d206fbe10d892569aa2028ddc074b431d0536
yuhwani <cndqnr346@gmail.com>
2026-04-05 21:25:18 +0900

    5주차3

--- fastapi-app/tests/test_api_deploy.py
+++ fastapi-app/tests/test_api_deploy.py
@@ -1,9 +1,10 @@
+import os
 import requests
 
-BASE_URL = "http://163.239.77.77:8011"
+BASE_URL = os.getenv("BASE_URL", "http://163.239.77.77:8011")
 
 def test_root():
-    response = requests.get(BASE_URL)
+    response = requests.get(f"{BASE_URL}/")
     assert response.status_code == 200
 
 def test_get_todos():
